90a312ca80306f4dc49397182e161d4d2467f0ff
Zallom <44950613+Zallom@users.noreply.github.com>
2019-11-11 17:07:24 +0100

    Update channel.py

--- channel/channel.py
+++ channel/channel.py
@@ -6,8 +6,13 @@ class Channel(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.channelAttachments = bot.get_channel()
-
+        self.channelAttachments = int
+    
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    async def media(self, ctx: commands.Context, mediaid: int):
+        self.channelAttachments = bot.get_channel(mediaid)
+        
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id == self.channelAttachments.id:
