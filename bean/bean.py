acc773530576eddaf5875432a45eabc4bd9936f0
Leaf <discordleaf@protonmail.com>
2019-11-15 20:39:07 +0100

    yes.

--- bean/bean.py
+++ bean/bean.py
@@ -12,20 +12,24 @@ class Bean(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(manage_messages=True)
     async def bean(self, ctx, target: discord.Member, flags: str=None):
-        if flags != "-s":
-            await ctx.send('<:ItemBean:{}> Beaned **{}** (`{}`)'.format(self.beanEmoji.id, target, target.id))
-        else:
-            await ctx.message.delete()
+        if target.id != self.bot.id:
+            if flags != "-s":
+                await ctx.send('<:ItemBean:{}> Beaned **{}** (`{}`)'.format(self.beanEmoji.id, target, target.id))
+            else:
+                await ctx.message.delete()
 
-        try:
-            message = await self.bot.wait_for('message', timeout=60*5, check=lambda m: m.author == target and m.channel.guild == ctx.guild)
-        except asyncio.TimeoutError:
-            pass
-        else:
             try:
-                await message.add_reaction(self.beanEmoji)
-            except Forbidden:
-                await message.channel.send('<:ItemBean:{}>'.format(self.beanEmoji.id))
+                message = await self.bot.wait_for('message', timeout=60*5, check=lambda m: m.author == target and m.channel.guild == ctx.guild)
+            except asyncio.TimeoutError:
+                pass
+            else:
+                try:
+                    await message.add_reaction(self.beanEmoji)
+                except Forbidden:
+                    await message.channel.send('<:ItemBean:{}>'.format(self.beanEmoji.id))
+        else:
+            await ctx.send('lol no.')
+         
 
 def setup(bot):
     bot.add_cog(Bean(bot))
