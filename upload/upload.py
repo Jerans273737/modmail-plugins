0dadfe47e97187e3c41e92b70818091232b94646
IchiiSama <45918948+IchiiSama@users.noreply.github.com>
2019-11-23 17:51:30 +0100

    Update upload.py

--- upload/upload.py
+++ upload/upload.py
@@ -1,1 +1,18 @@
+import asyncio
+import discord
+from discord.ext import commands
 
+class Upload(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot      
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def upload(self, ctx, target: discord.Member, flags: str=None):
+        ctx.send("test")
+         
+
+def setup(bot):
+    bot.add_cog(Upload(bot))
