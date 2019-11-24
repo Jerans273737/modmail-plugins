1a25e64174ff2635e4099fc60822923c07dd215d
Leaf <discordleaf@protonmail.com>
2019-11-24 17:24:31 +0100

    Multiple urls

--- upload/upload.py
+++ upload/upload.py
@@ -12,11 +12,21 @@ class Upload(commands.Cog):
         message = ctx.message
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', links)
         if len(urls) > 0:
-            await ctx.channel.send(urls[0])
+            buff = ''
+            for url in urls:
+                if len(url) + len(buff) > 1990:
+                    await ctx.send('{}'.format(buff))
+                    buff = ''
+                buff += '\n{}'.format(url)
+            if not buff:
+                return
+            
+            return await ctx.send('{}'.format(buff))
         elif len(message.attachments) > 0:
             await ctx.channel.send(message.attachments[0])
         else:
             await ctx.channel.send("Merci de renseigner une image à upload")
+            
 
 def setup(bot):
     bot.add_cog(Upload(bot))
