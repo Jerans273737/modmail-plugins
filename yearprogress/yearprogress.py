cea7fcdb16629ed1dff494e661060a5f74446ffb
Leaf ▲ <discordleaf@protonmail.com>
2020-01-15 15:49:31 +0100

    debuging cuz im too dumb to see if it fails

--- yearprogress/yearprogress.py
+++ yearprogress/yearprogress.py
@@ -12,6 +12,7 @@ class YearProgress(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(embed_links=True)
     async def yprogress(self, ctx: commands.Context):
+        print('wat')
         initial_date = date(date.today().year, 1, 1)
         year = date.today().year
         percent = math.floor((((date.today() - initial_date) / (1000 * 60 * 60 * 24)) * 100) / (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)) if 366 else 365))
