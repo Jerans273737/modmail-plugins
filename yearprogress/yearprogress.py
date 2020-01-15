21929b08cd140bce4439554e22a4f96cd0613e94
Leaf ▲ <discordleaf@protonmail.com>
2020-01-15 15:51:53 +0100

    k plz hit me

--- yearprogress/yearprogress.py
+++ yearprogress/yearprogress.py
@@ -8,14 +8,16 @@ class YearProgress(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def is_leap_year(year): 
+        return (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0))
+
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(embed_links=True)
     async def yprogress(self, ctx: commands.Context):
-        print('wat')
         initial_date = date(date.today().year, 1, 1)
         year = date.today().year
-        percent = math.floor((((date.today() - initial_date) / (1000 * 60 * 60 * 24)) * 100) / (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)) if 366 else 365))
+        percent = math.floor((((date.today() - initial_date) / (1000 * 60 * 60 * 24)) * 100) / (self.is_leap_year(date.today().year) if 366 else 365))
         year_bar = ''
 
         for i in range(5, 100, 5):
