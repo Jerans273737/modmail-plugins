8a52c104d20746014a026099f7e9f5a2837b656f
l e a f <discordleaf@protonmail.com>
2020-04-10 18:09:07 +0200

    test

--- invite-recap/invite-recap.py
+++ invite-recap/invite-recap.py
@@ -2,21 +2,20 @@ from discord import Member
 from discord.ext import commands
 import asyncio
 
-class MyCog(commands.Cog):
-  async def recap(self):
-    while True:
-        await self.bot.get_channel(recap_channel).send('test')
-        await asyncio.sleep(10)    
+class InviteRecap(commands.Cog):
   
-  def __init__(self, bot):
+  async def __init__(self, bot):
     self.bot = bot
     self.joins = 0
     self.recap_channel = 698185668047667232
-    self.bot.loop.create_task(self.recap())
+
+    while True:
+        await self.bot.get_channel(recap_channel).send('test')
+        await asyncio.sleep(10)
 
   @commands.Cog.listener()
   async def on_member_join(self, member):
     self.joins = self.joins + 1
 
 def setup(bot):
-  bot.add_cog(MyCog(bot))
+  bot.add_cog(InviteRecap(bot))
