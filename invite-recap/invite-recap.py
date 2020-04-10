77e40edecbd7ae3dd43456c6b1d858a4e27bfe13
l e a f <discordleaf@protonmail.com>
2020-04-10 17:56:34 +0200

    imamess

--- invite-recap/invite-recap.py
+++ invite-recap/invite-recap.py
@@ -3,20 +3,20 @@ from discord.ext import commands
 import asyncio
 
 class MyCog(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-    self.joins = 0
-    self.recap_channel = 698185668047667232
-
   async def recap(self):
     while True:
         await self.bot.get_channel(recap_channel).send(f'> **{joins}** nouveaux membres')
         self.joins = 0
-        await asyncio.sleep(300)
+        await asyncio.sleep(300)    
+  
+  def __init__(self, bot):
+    self.bot = bot
+    self.joins = 0
+    self.recap_channel = 698185668047667232
+    self.bot.loop.create_task(self.recap())
 
   @commands.Cog.listener()
   async def on_member_join(self, member):
-    self.bot.loop.create_task(self.recap())
     self.joins = self.joins + 1
 
 def setup(bot):
